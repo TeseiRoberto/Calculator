351c3a46da72b988a6886091890fd5c4ebde8133
Tesei Roberto <roberto_tesei@yahoo.com>
2023-01-07 13:29:29 +0100

    First commit

---
+++ build.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+SRCS=src/*.java
+javac --module-path "javafx-lib-x64/" --add-modules javafx.controls,javafx.fxml $SRCS
---
+++ run.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+java --module-path "javafx-lib-x64/" --add-modules javafx.controls,javafx.fxml Main
