07ecd5b7288eea4f79e1ba0e3a77bdf0ae26b700
Tesei Roberto <roberto_tesei@yahoo.com>
2023-01-07 23:33:12 +0100

    Added package name

--- run.sh
+++ run.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-java --module-path "javafx-lib-x64/" --add-modules javafx.controls,javafx.fxml Main
+PACKAGE_NAME="com.myPackage"
+
+java --module-path "javafx-lib-x64/" --add-modules javafx.controls,javafx.fxml -cp bin $PACKAGE_NAME.Main
